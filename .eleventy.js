6fb5145c11fa6cd9447364b656b57472571f45e3
Miriam Suzanne <miriam@oddbird.net>
2022-05-11 14:59:11 -0600

    Remove dynamic loading of admin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,3 @@
-const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
 const yaml = require('js-yaml');
 const util = require('util')
 const _ = require('lodash');
@@ -89,13 +88,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addDataExtension('yaml', yaml.load);
   eleventyConfig.setQuietMode(true);
   eleventyConfig.setDataDeepMerge(true);
-
-  // serverless
-  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
-    name: 'dynamic', // The serverless function name from your permalink object
-    copy: ['./filters/', './utils/', './client-config.js'],
-    redirects: false,
-  });
 
   // settings
   return {
